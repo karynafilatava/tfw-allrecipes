8be73383b27508e96d561d566570c3a81b9419ab
Karyna Filatava <karinafilatowa@yandex.ru>
2017-08-01 08:51:19 +0300

    Final preparations

--- step_definitions/browser_steps.js
+++
@@ -1,8 +0,0 @@
-var { defineSupportCode } = require('cucumber');
-
-defineSupportCode(function({ Given, When, Then }) {
-
-   	// Then(/^I should see \'search\' in title$/, function () {
-
-   	// });
-});
